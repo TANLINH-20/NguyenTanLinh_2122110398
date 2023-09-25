b13b791a92764340e24227a1579dc72845a788b4
Admin <Admin@D20906>
2023-09-25 15:52:03 +0700

    add

--- NguyenTanLinh_2122110398/Form2.cs
+++ NguyenTanLinh_2122110398/Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,18 +26,35 @@ namespace NguyenTanLinh_2122110398
         private void button1_Click(object sender, EventArgs e)
         {
             string gender = "";
+
+            if (textBox1.Text == "" || comboBox1.Text == "" || (!radioButton1.Checked && !radioButton2.Checked))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+                return;
+            }
+
             if (radioButton1.Checked)
             {
                 gender = radioButton1.Text;
             }
-            if(radioButton2.Checked)
+
+            if (radioButton2.Checked)
             {
                 gender = radioButton2.Text;
             }
-            richTextBox1.Text = "Họ tên: "+textBox1.Text + "\n" + 
-                                "Ngày sinh: "+dateTimePicker1.Text + "\n" + 
-                                "Giới tinh: "+gender + "\n" + 
-                                "Khoa: "+comboBox1.Text+"\n";
+
+            string oldData = richTextBox1.Text;
+            string newData = "Họ tên: " + textBox1.Text + "\n" +
+                             "Ngày sinh: " + dateTimePicker1.Text + "\n" +
+                             "Giới tính: " + gender + "\n" +
+                             "Khoa: " + comboBox1.Text + "\n";
+            richTextBox1.Text = oldData + newData;
+            textBox1.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            comboBox1.SelectedIndex = -1;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
         }
+
     }
 }
